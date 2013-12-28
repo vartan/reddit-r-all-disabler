d2666aff415dcff3e3140306bdea78d57a6b6f07
Michael Vartan <admin@mvartan.com>
2013-12-27 18:15:05 -0800

    changed /r/all behavior

--- reddit-r-all-disabler.user.js
+++ reddit-r-all-disabler.user.js
@@ -7,8 +7,9 @@
 // @copyright    Michael Vartan, 2012-2013
 // ==/UserScript==
 if(document.URL.indexOf("/r/all")!=-1)
-	window.location = "http://www.google.com/"
+	document.getElementById("siteTable").innerHTML="/r/all is currently disabled.";
 var anchors = document.getElementsByTagName('a');
 for(var i = 0; i < anchors.length; i++)
     if(anchors[i] != null && anchors[i].getAttribute("href").indexOf("/r/all")!=-1)
 		anchors[i].parentNode.removeChild(anchors[i]);
+
